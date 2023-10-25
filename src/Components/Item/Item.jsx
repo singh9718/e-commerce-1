bf48bf2f1cc636bb7db0b44baf5cf2f034aed310
Roshan Singh <singh.roshan9718@gmail.com>
2023-10-25 13:43:15 +0530

    added components for homepage and designed it

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './Item.css'
+import React from "react";
+import "./Item.css";
 
-const Item = () => {
+const Item = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
 
-export default Item
+      <div className="item-prices">
+        <div className="item-price-new">${props.new_price}</div>
+        <div className="item-price-old">${props.old_price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
