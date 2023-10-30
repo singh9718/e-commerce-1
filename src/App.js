5d481b418808f40d1a32d9bc23a62e533db84805
Roshan Singh <singh.roshan9718@gmail.com>
2023-10-30 15:49:20 +0530

    added productdisplay and decorated it

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
           <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path='/product' element={<Product />}>
-            <Route path='productId' element={<Product />} />
+            <Route path='/product/:productId' element={<Product />} />
           </Route>
 
           <Route path='/cart' element={<Cart />} />
